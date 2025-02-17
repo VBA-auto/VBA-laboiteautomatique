963751c2c8e1d509b9727b5a0c8104fd41c14281
Chowon-Hasan <chowonhasan7@gmail.com>
2025-02-17 16:45:35 +0600

    dasds

--- src/app/confirmation/page.jsx
+++ src/app/confirmation/page.jsx
@@ -11,7 +11,7 @@ const ConfirmationPageContent = () => {
 
   useEffect(() => {
     if (sessionId) {
-      fetch("/app/api/checkout-session", {
+      fetch("/api/checkout-session", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
