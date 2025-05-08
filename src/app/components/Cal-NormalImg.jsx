51667a37a1aa48303a21a11a77e1309cf42039a0
Chowon-Hasan <chowonhasan7@gmail.com>
2025-05-08 16:34:05 +0600

    new images order

--- src/app/components/Cal-NormalImg.jsx
+++ src/app/components/Cal-NormalImg.jsx
@@ -2,9 +2,9 @@ const VehicleImages = () => {
   return [
     "https://laboiteautomatique.com/images/cal-normal-0.webp",
     "https://laboiteautomatique.com/images/cal-normal-1.webp",
-    "https://laboiteautomatique.com/images/cal-normal-2.webp",
     "https://laboiteautomatique.com/images/cal-normal-3.webp",
     "https://laboiteautomatique.com/images/cal-normal-4.webp",
+    "https://laboiteautomatique.com/images/cal-normal-2.webp",
     "https://laboiteautomatique.com/images/cal-normal-5.webp",
     "https://laboiteautomatique.com/images/cal-normal-6.webp",
     "https://laboiteautomatique.com/images/cal-normal-7.webp",
