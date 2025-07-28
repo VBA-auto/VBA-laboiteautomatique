910db57528849df029a9a3fd7f952a397b3c35e1
chowon-hasan <chowonhasan7@gmail.com>
2025-07-28 22:31:57 +0600

    hdjgfhgjdfs

--- src/app/components/DynamicPopUp/DynamicPopup.jsx
+++ src/app/components/DynamicPopUp/DynamicPopup.jsx
@@ -9,6 +9,8 @@ export default function DynamicPopup() {
   const [isVisible, setIsVisible] = useState(false);
   const [timeLeft, setTimeLeft] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [loadingPrice, setLoadingPrice] = useState(true);
+  const [promoPrice, setPromoPrice] = useState(0);
 
   // Countdown calculation
   const calculateTimeLeft = (targetDate) => {
@@ -72,10 +74,30 @@ export default function DynamicPopup() {
     setIsVisible(false);
   };
 
+  useEffect(() => {
+    const PriceFetchPromo = async () => {
+      try {
+        const res = await fetch("/api/promoCode");
+        const data = await res.json();
+        const activePromo = data.find((promo) => promo.status);
+
+        if (activePromo) {
+          setPromoPrice(activePromo.price);
+        }
+      } catch (err) {
+        console.error("Promo fetch failed", err);
+      } finally {
+        setLoadingPrice(false);
+      }
+    };
+
+    PriceFetchPromo();
+  }, []);
+
   if (loading || !isVisible || !timeLeft) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50 p-4">
+    <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-end z-50 p-4">
       <div className="bg-white rounded-lg shadow-xl max-w-2xl w-full relative overflow-hidden">
         {/* Close Button */}
         <button
@@ -98,10 +120,20 @@ export default function DynamicPopup() {
           </div>
 
           {/* Countdown Timer */}
-          <div className="bg-blue-50 py-3 px-4 rounded-lg text-center mb-4 font-semibold text-[#c33e1d]">
+          <div className="bg-blue-50 py-3 px-4 rounded-lg text-center mb-4 font-semibold text-gray-800">
             {/* Time remaining:{" "} */}
-            <p className="text-4xl">{`${timeLeft.days}j ${timeLeft.hours}h ${timeLeft.minutes}m`}</p>
-            {/* <p className="text-4xl">{`${timeLeft.days}j ${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`}</p> */}
+            <p className="text-3xl text-indigo-950">{`${timeLeft.days}j ${timeLeft.hours}h ${timeLeft.minutes}m`}</p>
+            <h1 className="text-xl text-blue-500 mb-1 inline-flex items-center gap-2">
+              <span className="text-gray-700">obtenir moins:</span>
+              <span>
+                {loadingPrice ? (
+                  <span className="w-5 h-5 border-2 border-blue-500 border-t-transparent rounded-full animate-spin inline-block" />
+                ) : (
+                  -promoPrice
+                )}{" "}
+                €
+              </span>
+            </h1>
           </div>
 
           <div className="flex justify-center my-4">
